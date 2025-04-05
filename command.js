45fde62fb08fff46a4c14a39217b7a3b50a86f1f
BIG BUG <152616194+BIGSUS24@users.noreply.github.com>
2025-04-05 10:13:27 +0530

    fix

--- command.js
+++ command.js
@@ -7,12 +7,12 @@ const commands = [
   },
 ];
 
-const rest = new REST({ version: '10' }).setToken('MTI3MjUyNjM3NDc4NzIyMzU1NQ.Gfq7aA.U44W6KbuzRL3TFNyzaTz_2TOwFHroeyrsf122c');
+const rest = new REST({ version: '10' }).setToken('Put your Own Token');
 
 try {
   console.log('Started refreshing application (/) commands.');
 
-  await rest.put(Routes.applicationCommands("1272526374787223555"), { body: commands });
+  await rest.put(Routes.applicationCommands("Put channel id here"), { body: commands });
 
   console.log('Successfully reloaded application (/) commands.');
 } catch (error) {
